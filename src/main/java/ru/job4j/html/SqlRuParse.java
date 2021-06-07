d9553d33afd07cb0e5fa4225653fcb6d759c18bd
18485888 <AAlUstinov@sberbank.ru>
2021-06-08 01:00:50 +0300

    Парсинг HTML страницы

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,20 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+        }
+        Element rowDate = doc.select(".altCol").get(1);
+        System.out.println(rowDate.text());
+    }
+}
