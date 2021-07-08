612780eb9280864375e57c09f37bb158e36c20c8
18485888 <AAlUstinov@sberbank.ru>
2021-07-09 00:49:08 +0300

    PsqlStore

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -3,8 +3,12 @@ package ru.job4j.grabber;
 import ru.job4j.grabber.utils.Post;
 
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.sql.Date;
 
 public class PsqlStore implements Store, AutoCloseable {
 
@@ -19,18 +23,31 @@ public class PsqlStore implements Store, AutoCloseable {
         cnn = DriverManager.getConnection(cfg.getProperty("url"), cfg.getProperty("login"), cfg.getProperty("password"));
     }
 
-    @Override
-    public void save(Post post) {
-        try(PreparedStatement statement = cnn.prepareStatement(
-                "INSERT INTO post(name, text, link, created) values (?, ?, ?, ?)"), Statement.RETURN_GENERATED_KEYS)) {
-                sta
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try (PreparedStatement statement =
-                     cnn.prepareStatement("insert into cities(name, population) values (?, ?)",
-                             Statement.RETURN_GENERATED_KEYS)) {
+    public Date convertToDate(LocalDateTime dateToConvert) {
+        return (Date) Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+    }
 
+    public LocalDateTime convertToLocalDate(Date dateToConvert) {
+        return dateToConvert.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+    }
+
+    @Override
+    public void save(Post post) throws SQLException {
+        try(PreparedStatement statement =
+                    cnn.prepareStatement("INSERT INTO post(name, text, link, created) values (?, ?, ?, ?)",
+                            Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getDescription());
+            statement.setString(3, post.getLink());
+            statement.setDate(4, convertToDate(post.getCreated()));
+            statement.execute();
+            try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    post.setId(resultSet.getInt("id"));
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -38,12 +55,41 @@ public class PsqlStore implements Store, AutoCloseable {
 
     @Override
     public List<Post> getAll() {
-        return null;
+        List<Post> result = new ArrayList<>();
+        try (PreparedStatement statement = cnn.prepareStatement("SELECT * FROM post");
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                result.add(
+                        new Post(resultSet.getInt("id"),
+                            resultSet.getString("title"),
+                            resultSet.getString("link"),
+                            resultSet.getString("description"),
+                            convertToLocalDate(resultSet.getDate("created"))
+                        )
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
     public Post findById(int id) {
-        return null;
+        Post result = new Post();
+        try (PreparedStatement statement = cnn.prepareStatement("SELECT * FROM post WHERE id = ?");
+                ResultSet resultSet = statement.executeQuery()) {
+            while(resultSet.next()) {
+                result.setId(resultSet.getInt("id"));
+                result.setTitle(resultSet.getString("title"));
+                result.setLink(resultSet.getString("link"));
+                result.setDescription(resultSet.getString("description"));
+                result.setCreated(convertToLocalDate(resultSet.getDate("create")));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
--- src/main/java/ru/job4j/grabber/utils/Post.java
+++ src/main/java/ru/job4j/grabber/utils/Post.java
@@ -8,4 +8,54 @@ public class Post {
     private String link;
     private String description;
     private LocalDateTime created;
+
+    public Post() {}
+
+    public Post(Integer id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
 }
