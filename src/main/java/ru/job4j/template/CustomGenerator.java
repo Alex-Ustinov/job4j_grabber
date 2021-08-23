ee62b98f730a34a7392e14f3567dc58fc8d5853d
18485888 <AAlUstinov@sberbank.ru>
2021-08-24 00:17:51 +0300

    Что такое TDD

---
+++ src/main/java/ru/job4j/template/CustomGenerator.java
@@ -0,0 +1,13 @@
+package ru.job4j.template;
+
+import java.util.Map;
+
+public class CustomGenerator implements Generator {
+
+    @Override
+    public String produce(String template, Map<String, String> args) {
+        for () {
+
+        }
+    }
+}
