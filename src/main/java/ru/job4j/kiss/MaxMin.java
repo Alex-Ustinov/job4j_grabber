40b0fbae3c4127001377602d9e5f2877fe8d415a
18485888 <AAlUstinov@sberbank.ru>
2021-08-29 00:25:37 +0300

    Принципы Kiss, Dry и Yagni

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -7,28 +7,11 @@ import java.util.function.Predicate;
 
 public class MaxMin {
 
-    public <T> boolean customCompare (T firstValue, T secondValue, Comparator<T> comparator,  String rate) {
-        if (rate.equals("max")) {
-            return comparator.compare(firstValue, secondValue) > 0;
-        } else if (rate.equals("min")) {
-            return comparator.compare(firstValue, secondValue) < 0;
-        }
-        return false;
-    }
-
-    public <T> T setInitialValue (List<T> value) {
-        return value.size() != 0 ? value.get(0) : null;
-    }
-    public <T> T setInitialValuePredicate (List<T> value) {
-        Predicate<List> predicate = (list) -> list.size() != 0;
-        return predicate.test(value) ? value.get(0) : null;
-    }
-
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        T result = setInitialValuePredicate(value);
+        T result = value.size() != 0 ? value.get(0) : null;
         if (!result.equals(null)) {
             for (int i = 1; i < value.size(); i++) {
-                if (customCompare(result, value.get(i), comparator, "max")) {
+                if (comparator.compare(result, value.get(i)) > 0) {
                     result = value.get(i);
                 }
             }
@@ -37,10 +20,10 @@ public class MaxMin {
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        T result = setInitialValuePredicate(value);
+        T result = value.size() != 0 ? value.get(0) : null;
         if (!result.equals(null)) {
             for (int i = 1; i < value.size(); i++) {
-                if (customCompare(result, value.get(i), comparator, "min")) {
+                if (comparator.compare(result, value.get(i)) < 0) {
                     result = value.get(i);
                 }
             }
@@ -48,15 +31,4 @@ public class MaxMin {
         return result;
     }
 
-    public <T> T myOwnMaxMin(List<T> value, BiPredicate biPredicate) {
-        T result = setInitialValue(value);
-        if (!result.equals(null)) {
-            for (int i = 1; i < value.size(); i++) {
-                if (biPredicate.test(value.get(i), result)) {
-                    result = value.get(i);
-                }
-            }
-        }
-        return result;
-    }
 }
--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -11,27 +11,6 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class MaxMinTest {
-    @Test
-    public void whenMyOwnMaxMinTestMax() {
-        MaxMin maxMin = new MaxMin();
-        List customList = new ArrayList();
-        customList.add(5);
-        customList.add(1);
-        customList.add(2);
-        BiPredicate<Integer, Integer> biPredicate = (x, y) -> x > y;
-        assertThat(maxMin.myOwnMaxMin(customList, biPredicate), is(5));
-    }
-
-    @Test
-    public void whenMyOwnMaxMinTestMin() {
-        MaxMin maxMin = new MaxMin();
-        List customList = new ArrayList();
-        customList.add(7);
-        customList.add(1);
-        customList.add(2);
-        BiPredicate<Integer, Integer> biPredicate = (x, y) -> x < y;
-        assertThat(maxMin.myOwnMaxMin(customList, biPredicate), is(1));
-    }
 
     @Test
     public void maxTest() {
