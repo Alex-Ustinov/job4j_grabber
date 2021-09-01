a55d8fbe6c3287340afd2fe31d6f16655cafc7b6
18485888 <AAlUstinov@sberbank.ru>
2021-09-01 23:40:11 +0300

    Принципы Kiss, Dry и Yagni

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -2,28 +2,25 @@ package ru.job4j.kiss;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 public class MaxMin {
 
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        T result = value.size() != 0 ? value.get(0) : null;
-        if (!result.equals(null)) {
-            for (int i = 1; i < value.size(); i++) {
-                if (comparator.compare(result, value.get(i)) > 0) {
-                    result = value.get(i);
-                }
-            }
-        }
-        return result;
+        Predicate<Integer> predicate = (x) -> x < 0;
+        return minMax(value, predicate, comparator);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
+        Predicate<Integer> predicate = (x) -> x > 0;
+        return minMax(value, predicate, comparator);
+    }
+
+    private <T> T minMax(List<T> value, Predicate<Integer> predicate, Comparator<T> comparator) {
         T result = value.size() != 0 ? value.get(0) : null;
         if (!result.equals(null)) {
             for (int i = 1; i < value.size(); i++) {
-                if (comparator.compare(result, value.get(i)) < 0) {
+                if (predicate.test(comparator.compare(value.get(i), result))) {
                     result = value.get(i);
                 }
             }
